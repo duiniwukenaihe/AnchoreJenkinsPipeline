ad528ad1024971d64ae33ed03af5f6fdf1426a3a
Jimmy <jimmyjamesbaldwin@gmail.com>
2019-05-27 10:04:31 +0100

    Move Jenkins Dockerfile out into separate file as echo-ing it out from the install_jenkins.sh shell script caused formatting problems

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -1,25 +1,16 @@
 #!/bin/bash
 
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic test"
+sudo apt-get update && sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+clear
+
 # check if we've already got a jenkins container running
 if ! sudo docker ps | grep -q 'jenkins-docker'; then
    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic test"
-   sudo apt-get update && sudo apt-get install -y docker-ce docker-ce-cli containerd.io default-jre
-   echo "
-   from jenkinsci/jenkins
-    
-   USER root
-   RUN apt-get update -qq \
-       && apt-get install -qqy apt-transport-https ca-certificates curl gnupg2 software-properties-common 
-   RUN curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
-   RUN add-apt-repository \
-      "deb [arch=amd64] https://download.docker.com/linux/debian \
-      $(lsb_release -cs) \
-      stable"
-   RUN apt-get update  -qq \
-       && apt-get install docker-ce=17.12.1~ce-0~debian -y
-   RUN usermod -aG docker jenkins" > Dockerfile
-   sudo docker image build -t jenkins-docker .
+   sudo apt-get install -y default-jre
+   sudo docker image build -f Dockerfile_jenkins -t jenkins-docker .
    sudo docker container run -d -p 443:8080 -v /var/run/docker.sock:/var/run/docker.sock jenkins-docker
    sleep 2
 fi
